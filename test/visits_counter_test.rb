f84773bc4890ae7865e65671773637151aaddafa
Murad Iusufov <flood4life@gmail.com>
2021-05-22 15:26:05 +0200

    refactor(test): helper method for comparing lines and stats

--- test/visits_counter_test.rb
+++ test/visits_counter_test.rb
@@ -6,31 +6,33 @@ require_relative '../lib/visits_counter'
 
 class TestVisitsCounter < Minitest::Test
   def test_record_single_visit
-    counter = VisitsCounter.new
     lines = [
       Line.new(path: '/foo', ip: '127.0.0.1')
     ]
+    stats = [
+      PathStats.new(path: '/foo', total: 1, unique: 1)
+    ]
 
-    lines.each { counter.record(_1) }
-
-    stats = counter.stats
-    expected_stats = [PathStats.new(path: '/foo', total: 1, unique: 1)]
-
-    assert_equal stats, expected_stats
+    assert_lines_and_stats(lines, stats)
   end
 
   def test_record_two_visits_from_same_ip
-    counter = VisitsCounter.new
     lines = [
       Line.new(path: '/foo', ip: '127.0.0.1'),
       Line.new(path: '/foo', ip: '127.0.0.1')
     ]
+    stats = [
+      PathStats.new(path: '/foo', total: 2, unique: 1)
+    ]
 
+    assert_lines_and_stats(lines, stats)
+  end
+
+  private
+
+  def assert_lines_and_stats(lines, stats)
+    counter = VisitsCounter.new
     lines.each { counter.record(_1) }
-
-    stats = counter.stats
-    expected_stats = [PathStats.new(path: '/foo', total: 2, unique: 1)]
-
-    assert_equal stats, expected_stats
+    assert_equal stats, counter.stats
   end
 end
