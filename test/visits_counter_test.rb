969a9de9c8af6b2fd3c53d8fc5b8c292f3ffda56
Murad Iusufov <flood4life@gmail.com>
2021-05-22 15:23:16 +0200

    test: two visits from same ip

--- test/visits_counter_test.rb
+++ test/visits_counter_test.rb
@@ -7,12 +7,29 @@ require_relative '../lib/visits_counter'
 class TestVisitsCounter < Minitest::Test
   def test_record_single_visit
     counter = VisitsCounter.new
-    line1 = Line.new(path: '/foo', ip: '127.0.0.1')
+    lines = [
+      Line.new(path: '/foo', ip: '127.0.0.1')
+    ]
 
-    counter.record(line1)
+    lines.each { counter.record(_1) }
 
     stats = counter.stats
     expected_stats = [PathStats.new(path: '/foo', total: 1, unique: 1)]
+
+    assert_equal stats, expected_stats
+  end
+
+  def test_record_two_visits_from_same_ip
+    counter = VisitsCounter.new
+    lines = [
+      Line.new(path: '/foo', ip: '127.0.0.1'),
+      Line.new(path: '/foo', ip: '127.0.0.1')
+    ]
+
+    lines.each { counter.record(_1) }
+
+    stats = counter.stats
+    expected_stats = [PathStats.new(path: '/foo', total: 2, unique: 1)]
 
     assert_equal stats, expected_stats
   end
