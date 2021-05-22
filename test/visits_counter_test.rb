4ecaa247d120fc301931507ca53ebecc73574a35
Murad Iusufov <flood4life@gmail.com>
2021-05-22 15:31:48 +0200

    test: more test for visits counter

--- test/visits_counter_test.rb
+++ test/visits_counter_test.rb
@@ -28,6 +28,31 @@ class TestVisitsCounter < Minitest::Test
     assert_lines_and_stats(lines, stats)
   end
 
+  def test_record_two_visits_from_different_ips
+    lines = [
+      Line.new(path: '/foo', ip: '127.0.0.1'),
+      Line.new(path: '/foo', ip: '192.168.1.1')
+    ]
+    stats = [
+      PathStats.new(path: '/foo', total: 2, unique: 2)
+    ]
+
+    assert_lines_and_stats(lines, stats)
+  end
+
+  def test_record_different_paths_same_ip
+    lines = [
+      Line.new(path: '/foo', ip: '127.0.0.1'),
+      Line.new(path: '/bar', ip: '127.0.0.1')
+    ]
+    stats = [
+      PathStats.new(path: '/foo', total: 1, unique: 1),
+      PathStats.new(path: '/bar', total: 1, unique: 1)
+    ]
+
+    assert_lines_and_stats(lines, stats)
+  end
+
   private
 
   def assert_lines_and_stats(lines, stats)
