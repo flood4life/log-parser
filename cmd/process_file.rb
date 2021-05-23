7ddcf0c65f6874f49a880a4f39bdb60532138c11
Murad Iusufov <flood4life@gmail.com>
2021-05-23 14:26:49 +0200

    refactor: use StdoutPresenter in ProcessFile

--- cmd/process_file.rb
+++ cmd/process_file.rb
@@ -2,7 +2,7 @@
 
 require_relative '../lib/line_parser'
 require_relative '../lib/visits_counter'
-require_relative '../lib/stats_sorter'
+require_relative '../lib/stdout_presenter'
 
 # @ProcessFile is a command that orchestrates reading the webserver log,
 # parsing each line, and printing the sorted stats to STDOUT.
@@ -14,30 +14,29 @@ class ProcessFile
 
   def call
     record_each_line
+    @presenter = StdoutPresenter.new(counter.stats)
     print_top_total_views
     print_top_unique_views
   end
 
   private
 
+  attr_reader :counter, :presenter, :file_path
+
   def print_top_total_views
     puts '--- TOP TOTAL VIEWS ---'
-    StatsSorter.new(@counter.stats).by_total.each do |stat|
-      puts "#{stat.path} #{stat.total}"
-    end
+    puts presenter.total
     puts '--- END TOP TOTAL VIEWS ---'
   end
 
   def print_top_unique_views
     puts '--- TOP UNIQUE VIEWS ---'
-    StatsSorter.new(@counter.stats).by_unique.each do |stat|
-      puts "#{stat.path} #{stat.unique}"
-    end
+    puts presenter.unique
     puts '--- END TOP UNIQUE VIEWS ---'
   end
 
   def record_each_line
-    File.foreach(@file_path) do |line|
+    File.foreach(file_path) do |line|
       parsed_line = LineParser.new(line).call
       @counter.record(parsed_line)
     end
